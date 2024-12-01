42ddbc1181992dcbb12dd76a1aa893ab46340cad
CabirSultanov <antosickartosic@gmail.com>
2024-12-01 23:14:52 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('#menu li').on('mouseenter', (e) => {
-        $(e.currentTarget).children('ul').fadeIn(200);
-    });
-
-    $('#menu li').on('mouseleave', (e) => {
-        $(e.currentTarget).children('ul').fadeOut(200);
-    });
-});
